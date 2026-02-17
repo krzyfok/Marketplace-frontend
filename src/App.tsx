97b3142b76ec7ce3a4825c6df795d8e81c8aed32
krzyfok <k.fokczynski@gmail.com>
2026-02-17 19:44:37 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+export default function App() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <header className="bg-emerald-700 text-white p-4 font-sans" >
+        <div className="flex flex-row justify-between" >       
+          <h1 className=" text-6xl font-extrabold"> Marketplace</h1> 
+          
+            <button className="bg-white hover:bg-green-500 px-4 text-black rounded-full px-10 text-3xl font-bold">Log in</button>
+
+      </div> 
+      </header>
+
+      <footer className="text-black mt-auto">
+        <div>
+          <h1>AA</h1>
+        </div>
+      </footer>
+    </div>
+  );
+}
