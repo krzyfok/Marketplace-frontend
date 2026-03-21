dbc4b505fc9349e964f786ac0cd41d1565fc28ff
krzyfok <k.fokczynski@gmail.com>
2026-03-21 11:55:41 +0100

    move navabar to global components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import { Toaster } from "react-hot-toast";
-import { Navbar } from './pages/Home/components/Navbar';
+import { Navbar } from './components/Navbar';
 import {AuthModal} from './components/AuthModal'
 import { useAuth } from "./contexts/AuthContext";
 export default function App() {
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useAuth } from "../../../contexts/AuthContext";
+import { useAuth } from "../contexts/AuthContext";
 import { useState, Fragment } from "react";
 import { Dialog, TransitionChild, Transition, DialogPanel } from "@headlessui/react";
 
