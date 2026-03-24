a1fb90cab0ca6ddcd2b5030a6e4530088bba2e41
krzyfok <k.fokczynski@gmail.com>
2026-03-24 21:46:54 +0100

    feat: catalog page poc

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from "react"
+import { getAllProducts } from "../../services/ProductService";
+import type { Product } from "../../types/product";
 export default function CatalogPage (){
 
+    const [products, setProducts] = useState<Product[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState("");
 
+    useEffect(()=>{
+        const fetchProducts = async() => {
+            try{
+                const response = await getAllProducts();
+                
+                setProducts(response.products);
+            }
+             catch (err:any)
+            {
+                setError("ERROR");   
+            }
+            setIsLoading(false);
+
+        };
+        fetchProducts();
+    },[])
 
 
     return(
-        <div>a</div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+           {products.map((product)=>(
+
+            <div key={product.id}> {product.name} </div>
+           ))}     
+        </div>
     )
 }
---
+++ src/services/ProductService.tsx
@@ -0,0 +1,8 @@
+import api from "../api/axios";
+
+export const getAllProducts = async ()=>{
+
+    const response = await api.get("products")
+
+    return response.data;
+}
